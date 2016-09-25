b5f4e14137ba41a913a4fd980cf555c12da265eb
Taff Gao <gaotongfei1995@gmail.com>
2016-09-25 21:12:08 +0800

    routing

--- example.py
+++ example.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
+@app.route('/user/<name>')
+def user(name):
+    return 'hello ' + name
+
+@app.route('/page/<int:page_id>')
+@app.route('/page/<page_id>')
+def page(page_id):
+    if isinstance(page_id, str):
+        return 'page id cannot be string type'
+    elif isinstance(page_id, int):
+        return 'page ' + str(page_id)
+
 if __name__ == '__main__':
     app.run(debug=True)
